cdbc348045117b0bf7856332fa2f512bf215c4f7
iso-sato <masumi.sato@isopra.co.jp>
2019-05-27 00:44:05 +0900

    認証処理・メンバー登録処理とテストコードを実装

---
+++ src/main/java/jp/co/isopra/lunchmap/service/LoginService.java
@@ -0,0 +1,66 @@
+package jp.co.isopra.lunchmap.service;
+
+import java.util.Collection;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.isopra.lunchmap.entity.AccountDetails;
+import jp.co.isopra.lunchmap.entity.Member;
+import jp.co.isopra.lunchmap.repositories.MemberRepository;
+
+/**
+ * ログイン処理のサービスクラス
+ * @author masumi.sato
+ *
+ */
+@Service
+public class LoginService implements UserDetailsService {
+
+	@Autowired
+	MemberRepository repository;
+
+	@Override
+	@Transactional(readOnly = true)
+	public UserDetails loadUserByUsername(String login_id)
+			throws UsernameNotFoundException {
+
+//		// DEBUG parameter出力
+//		java.lang.System.out.println("login:" + login_id);
+
+
+		//DBからユーザ情報を取得。
+		Member member = repository.findById(login_id).orElse(null);
+
+		if (Objects.isNull(member)) {
+			// DEBUG
+			java.lang.System.out.println("login:" + login_id + " NG");
+
+			// ユーザ情報が見つからなかった場合は例外を返却
+			throw new UsernameNotFoundException("User not found.");
+		}
+
+		// DEBUG
+		java.lang.System.out.println("login:" + login_id + " OK");
+
+		return new AccountDetails(member, getAuthorities(member));
+	}
+
+	/**
+	 * 認証が通った時にこのユーザに与える権限の範囲を設定する。
+	 * @param account DBから取得したユーザ情報。
+	 * @return 権限の範囲のリスト。
+	 */
+	private Collection<GrantedAuthority> getAuthorities(Member member) {
+		//認可が通った時にこのユーザに与える権限の範囲を設定する。
+		return AuthorityUtils.createAuthorityList("ROLE_MEMBER");
+	}
+
+}
--- src/test/java/jp/co/isopra/lunchmap/ApplicationTests.java
+++ src/test/java/jp/co/isopra/lunchmap/ApplicationTests.java
@@ -1,17 +1,18 @@
-package com.example.demo;
+package jp.co.isopra.lunchmap;
+
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class DemoApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
@@ -23,6 +24,6 @@ public class DemoApplicationTests {
 	@Test
 	public void homeResponse() {
 		String body = this.restTemplate.getForObject("/", String.class);
-		assertThat(body).isEqualTo("Spring is here!");
+		assertTrue(body.length() > 0);
 	}
 }
