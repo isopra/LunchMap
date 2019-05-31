c4ed973f7633f04fb59cf2484743d2d797729f8c
tomisaki.yui <tomisaki.yui@isopra.co.jp>
2019-05-31 17:00:49 +0900

    EntityとRepositoryの作成

--- src/main/java/jp/co/isopra/lunchmap/controller/FootController.java
+++ src/main/java/jp/co/isopra/lunchmap/controller/FootController.java
@@ -1,23 +1,32 @@
 package jp.co.isopra.lunchmap.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import jp.co.isopra.lunchmap.entity.FootPrint;
+import jp.co.isopra.lunchmap.repositories.FootPrintRepository;
+
+
 @Controller
 public class FootController {
 
+	@Autowired
+	FootPrintRepository repository;
+
 	@RequestMapping("/foot")
-	
+
 	public ModelAndView view(ModelAndView mav) {
-		
+
 		mav.setViewName("foot");
-		
+		Iterable<FootPrint> List = repository.findAll();
+
 		return mav;
-	
+
 	}
-	
-	
+
+
 
 
 
