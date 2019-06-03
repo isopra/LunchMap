7d5c00858b488dbfdee3d281d093b2dcdffd3c10
Jinchi-Haru <jinchi.haru@isopra.co.jp>
2019-06-03 17:17:57 +0900

    #19 アカウント情報表示完了

--- src/main/java/jp/co/isopra/lunchmap/controller/MenuController.java
+++ src/main/java/jp/co/isopra/lunchmap/controller/MenuController.java
@@ -1,7 +1,11 @@
 package jp.co.isopra.lunchmap.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.isopra.lunchmap.entity.AccountDetails;
 
 @Controller
 public class MenuController {
@@ -9,5 +13,14 @@ public class MenuController {
 	@RequestMapping("/menu")
 	public String showMenuPage() {
 		return "menu";
+	}
+
+	@RequestMapping("/menu/acount_edit")
+	public String showAccountEdit(@AuthenticationPrincipal AccountDetails accountDetails, Model model) {
+		model.addAttribute("id", accountDetails.getUsername());
+//		model.addAttribute("password", accountDetails.getMember().getPassword());
+		model.addAttribute("nickname", accountDetails.getMember().getNickname());
+
+		return "accountEdit";
 	}
 }
