5cc687e9b2836928af8362e03b78999ba7db30f6
tomisaki.yui <tomisaki.yui@isopra.co.jp>
2019-06-14 18:38:03 +0900

    不要コードの削除

--- src/main/java/jp/co/isopra/lunchmap/WebSecurityConfig.java
+++ src/main/java/jp/co/isopra/lunchmap/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     	// ログインなしでアクセスできるURLと、保護するURLを定義
         http.authorizeRequests()
-        .antMatchers("/member/register", "/login", "/authenticate", "/error", "/member/register/result", "/foot", "/foot/register").permitAll()
+        .antMatchers("/member/register", "/login", "/authenticate", "/error", "/member/register/result").permitAll()
         .antMatchers("/**").hasRole("MEMBER") // 権限によるアクセス範囲の制御。
         .anyRequest().authenticated()
         .and()
