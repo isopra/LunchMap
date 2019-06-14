bc8b6617fb7f5cda1d53c2482868555640612c28
takahashi-ryo <takahashi.ryo@isopra.co.jp>
2019-06-14 11:41:52 +0900

    本番環境での確認

--- src/main/java/jp/co/isopra/lunchmap/entity/FootPrint.java
+++ src/main/java/jp/co/isopra/lunchmap/entity/FootPrint.java
@@ -1,7 +1,5 @@
 package jp.co.isopra.lunchmap.entity;
 
-
-
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -9,84 +7,99 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
-
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-@Table(name="footprint")
-
+@Table(name = "footprint")
 public class FootPrint {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column
-	private Integer footprint_id;
+	private long footprint_id;
 
-	@Column(nullable=false)
+	@Column(length = 100)
+	@NotNull
 	private String place_id;
 
-	@Column(nullable=false)
+	@Column( length = 100)
+	@NotNull
 	private String login_id;
 
-	@Column(nullable = true)
+	@Column
 	private String comment;
 
-
-	@Column(nullable=false)
+	@Column
+	@NotNull
 	private Date created_time;
 
+	@ManyToOne
+	@JsonBackReference
+	private Member member;
+
+	@ManyToOne
+	@JsonBackReference
+	private Shop shop;
+
 	public FootPrint() {
-		this.created_time  = new Date();
+		this.created_time = new Date();
 	}
 
-
-	public Integer getFootprint_id() {
-		return footprint_id;
-	}
-
-	public void setFootprint_id(Integer footprint_id) {
+	public void setFootprint_id(long footprint_id) {
 		this.footprint_id = footprint_id;
 	}
 
-
-	public String getPlace_id() {
-		return place_id;
+	public long getFootprint_id() {
+		return footprint_id;
 	}
 
 	public void setPlace_id(String place_id) {
 		this.place_id = place_id;
 	}
 
-
-	public String getLogin_id() {
-		return login_id;
+	public String getPlace_id() {
+		return place_id;
 	}
 
 	public void setLogin_id(String login_id) {
 		this.login_id = login_id;
 	}
 
-
-	public String getComment() {
-		return comment;
+	public String getLogin_id() {
+		return login_id;
 	}
 
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
 
+	public String getComment() {
+		return comment;
+	}
+
 	public Date getCreated_time() {
 		return created_time;
 	}
 
-	public void setCreated_time(Date created_time) {
-		this.created_time = created_time;
+	public void setMember(Member member) {
+		this.member = member;
 	}
 
+	public Member getMember() {
+		return member;
+	}
 
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+
+	public Shop getShop() {
+		return shop;
+	}
 
 }
-
-
 
