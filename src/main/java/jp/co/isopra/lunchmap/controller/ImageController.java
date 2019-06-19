d3909f20ccbef232677f54b3ca80489e48896cba
tomisaki.yui <tomisaki.yui@isopra.co.jp>
2019-06-19 14:02:08 +0900

    画像登録

--- src/main/java/jp/co/isopra/lunchmap/controller/ImageController.java
+++ src/main/java/jp/co/isopra/lunchmap/controller/ImageController.java
@@ -5,7 +5,6 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +12,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import jp.co.isopra.lunchmap.entity.AccountDetails;
 import jp.co.isopra.lunchmap.entity.Image;
+import jp.co.isopra.lunchmap.entity.Shop;
 import jp.co.isopra.lunchmap.repositories.ImageRepository;
 import jp.co.isopra.lunchmap.repositories.ShopRepository;
 
@@ -34,7 +34,6 @@ public class ImageController {
 	{
 		mav.setViewName("image");
 		mav.addObject("place_name", place_name);
-
 		// viewで非表示
 		mav.addObject("place_id", place_id);
 
@@ -45,34 +44,50 @@ public class ImageController {
 	@RequestMapping(value="create/image/register")
 	public String createimg(
 			@RequestParam String place_id,
-//			@RequestParam String place_name,
-			@ModelAttribute Image image,
+			@RequestParam String place_name,
 			@AuthenticationPrincipal AccountDetails accountDetails,
 			ModelAndView mav
 			)
 	{
 		Image entity = new Image();
 		Date nowDate = new Date();
+		Shop entityShop;
 
-		System.out.println("place_id : " + place_id);
+		//Shop TableにPlace_idがない場合は登録
+		if(this.ShopRepository.existsById(place_id)) {
+			entityShop = this.ShopRepository.findById(place_id).get();
+		} else {
+			entityShop = new Shop();
 
-		/* 画像枚数分Insert
-		for() {
+			entityShop.setPlace_id(place_id);
+			entityShop.setPlace_name(place_name);
 
+			ShopRepository.save(entityShop);
 		}
-		*/
-//		Shop entityShop = this.ShopRepository.findById(place_id).get();
 
-		// DBに各値をセット
+		/*
+		 *  ToDo:画像枚数分Insert
+		 */
+
+		// image Tableにinsert
 		entity.setMember(accountDetails.getMember());
 		entity.setLogin_id(accountDetails.getMember().getLogin_id());
 		entity.setPlace_id(place_id);
 		entity.setCreated_time(nowDate);
-//		entity.setShop(entityShop);
+		entity.setShop(entityShop);
 
+		/* 画像保存場所の指定
 
+		String file_name = (place_id + "_" + entity.getImage_id());
+		String file_path = ("/images/{place_id}/file_name");
+
+		File imageFile = new File(file_path);
+
+		*/
 		imageRepository.save(entity);
-		return "redirect:/shopinfo/" + place_id;
+
+		System.out.println("place_id : " + place_id);
+		return "image";
 	}
 
 
