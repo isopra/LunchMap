007a19f5243634562a7e70a9e8e5c94eb332a8ae
KondoTakuya <kondo.takuya@isopra.co.jp>
2019-06-20 11:28:19 +0900

    本番環境での画像削除　パスを取得テスト

---
+++ src/main/java/jp/co/isopra/lunchmap/controller/PathController.java
@@ -0,0 +1,30 @@
+package jp.co.isopra.lunchmap.controller;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PathController {
+
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    public void index() throws Throwable {
+        String filepath = "static/images/ChIJD4tLmA-MGGARqHJvbMa-yNs"; // src/main/resources 配下の相対パス
+        Resource resource = resourceLoader.getResource("classpath:" + filepath);
+
+        String name = resource.getFilename();
+        System.out.println("name: " + name);
+
+        File file = resource.getFile();
+        System.out.println("file.exists(): " + file.exists());
+        System.out.println("file.getAbsolutePath(): " + file.getAbsolutePath());
+    }
+}
