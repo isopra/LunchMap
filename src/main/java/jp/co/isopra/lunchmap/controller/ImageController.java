9dfbc8ed7f6ddfedd4778da435cf7a3d58e06c8d
tomisaki.yui <tomisaki.yui@isopra.co.jp>
2019-06-20 20:59:17 +0900

    フォルダに画像を保存する

--- src/main/java/jp/co/isopra/lunchmap/controller/ImageController.java
+++ src/main/java/jp/co/isopra/lunchmap/controller/ImageController.java
@@ -1,5 +1,15 @@
 package jp.co.isopra.lunchmap.controller;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+import javax.imageio.IIOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -8,8 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jp.co.isopra.lunchmap.entity.AccountDetails;
 import jp.co.isopra.lunchmap.repositories.ImageRepository;
@@ -25,61 +33,6 @@ public class ImageController {
 	@Autowired
 	ShopRepository ShopRepository;
 
-//	// ファイル名指定
-//    private String getExtension(String filename) {
-//        int dot = filename.lastIndexOf(".");
-//        if (dot > 0) {
-//          return filename.substring(dot).toLowerCase();
-//        }
-//        return "";
-//      }
-//
-//      private String getUploadFileName(String fileName) {
-//
-//          return fileName + "_" +
-//                  DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS")
-//                      .format(LocalDateTime.now())
-//                  + getExtension(fileName);
-//      }
-//
-//    // フォルダの作成
-//	private void createDirectory() {
-//       Path path = Paths.get("images/");
-//        if (!Files.exists(path)) {
-//          try {
-//            Files.createDirectory(path);
-//          } catch (Exception e) {
-//            //エラー処理は省略
-//          }
-//        }
-//	 }
-//
-//	/* 保存場所の指定 */
-//    private void savefile(MultipartFile file) {
-//        String filename = getUploadFileName(file.getOriginalFilename());
-//        Path uploadfile = Paths.get("images/" + filename);
-//
-//        try (OutputStream os = Files.newOutputStream(uploadfile, StandardOpenOption.CREATE)) {
-//	      byte[] bytes = file.getBytes();
-//	      os.write(bytes);
-//        } catch (IIOException e) {
-//          // エラー処理は省略
-//        } catch (IOException e1) {
-//        	// TODO 自動生成された catch ブロック
-//			e1.printStackTrace();
-//		}
-//    }
-//
-//	// ファイルを保存
-//	private void savefiles(List<MultipartFile> multipartFiles)
-//	{
-//	      createDirectory();
-//	      for (MultipartFile file : multipartFiles)
-//	      {
-//	          savefile(file);
-//	      }
-//	 }
-
 	// 画像登録画面
 	@RequestMapping(value= "create/image", method = RequestMethod.GET)
     ModelAndView uploadview(
@@ -88,7 +41,6 @@ public class ImageController {
     		@RequestParam String place_name)
 	{
 
-//	      mav.addObject("form", new UploadForm());
 		mav.setViewName("image");
 	    mav.addObject("place_id", place_id);
 	    mav.addObject("place_name", place_name);
@@ -96,54 +48,21 @@ public class ImageController {
 	}
 
 	// 登録処理 ToDo:繰り返し
-	@RequestMapping(value="create/image/register", method = RequestMethod.POST, consumes = { "multipart/form-data" })
+	@RequestMapping(value="create/image/register", method = RequestMethod.POST)
 	String upload(
 			ModelAndView mav,
-			@RequestParam (value = "upfile") MultipartFile[] upfile,
-//			@RequestParam List<MultipartFile> upfile,
-//			@RequestParam String place_id,
-//			@RequestParam String place_name,
+			@RequestParam (value = "files") List<MultipartFile> files,
+			@RequestParam String place_id,
+			@RequestParam String place_name,
 			@AuthenticationPrincipal AccountDetails accountDetails
 			)
 	{
-		ObjectMapper mapper = new ObjectMapper();
-		
-		
 
-//		System.out.println(place_id);
-//		System.out.println(place_name);
-//		System.out.println(upfile.getFile());
-		for (MultipartFile file : upfile)
-		{
-			System.out.println(file.getOriginalFilename());
-		}
+		// 画像保存処理
+		savefiles(files, place_id);
 
-//		Image entity = new Image();
-//		Date nowDate = new Date();
-//		Shop entityShop;
-//
-//		//Shop TableにPlace_idがない場合は登録
-//		if(this.ShopRepository.existsById(place_id)) {
-//			entityShop = this.ShopRepository.findById(place_id).get();
-//		} else {
-//			entityShop = new Shop();
-//
-//			entityShop.setPlace_id(place_id);
-//			entityShop.setPlace_name(place_name);
-//
-//			ShopRepository.save(entityShop);
-//		}
-//
-//		// image Tableにinsert
-//		entity.setMember(accountDetails.getMember());
-//		entity.setLogin_id(accountDetails.getMember().getLogin_id());
-//		entity.setPlace_id(place_id);
-//		entity.setCreated_time(nowDate);
-//		entity.setShop(entityShop);
-//		imageRepository.save(entity);
-//
-//		savefiles(form.getFile());
-//
+
+
 		return "image";
 	}
 
@@ -157,5 +76,55 @@ public class ImageController {
 		return "redirect:/shopinfo/" + place_id;
 
 	}
+
+	/* 保存場所の指定 :ここでファイル名を変える*/
+    private void savefile(MultipartFile file, String place_id) {
+    	String filename = file.getOriginalFilename();
+    	Path uploadfile = Paths.get("images/" + place_id + "/" + filename);
+
+    	try (OutputStream os = Files.newOutputStream(uploadfile, StandardOpenOption.CREATE)) {
+    		byte[] bytes = file.getBytes();
+    		os.write(bytes);
+    	} catch (IIOException e) {
+    		// エラー処理は省略
+    	} catch (IOException e1) {
+    		// TODO 自動生成された catch ブロック
+    		e1.printStackTrace();
+    	}
+    }
+
+
+    //ファイルを保存　：ここで繰り返し処理 place_idも引数で受け取る
+    private void savefiles(List<MultipartFile> multipartFiles, String place_id) {
+    	createDirectory(place_id);
+    	for (MultipartFile file : multipartFiles)
+    	{
+    		savefile(file, place_id);
+    	}
+    }
+
+
+   // フォルダの作成
+	private void createDirectory(String place_id) {
+		Path path = Paths.get("images/");
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectory(path);
+			} catch (Exception e) {
+				//エラー処理は省略
+			}
+		}
+
+		Path path2 = Paths.get("images/" + place_id);
+		if (!Files.exists(path2)) {
+			try {
+				Files.createDirectory(path2);
+			} catch (Exception e) {
+				//エラー処理は省略
+			}
+		}
+
+	}
+
 }
 
