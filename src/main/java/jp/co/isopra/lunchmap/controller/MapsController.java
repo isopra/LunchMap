048f0199c6eda9820c490da902e6237226e605af
takahashi-ryo <takahashi.ryo@isopra.co.jp>
2019-06-21 18:07:13 +0900

    ajax及びcontrollerの修正

--- src/main/java/jp/co/isopra/lunchmap/controller/MapsController.java
+++ src/main/java/jp/co/isopra/lunchmap/controller/MapsController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
@@ -55,11 +54,91 @@ public class MapsController {
 	protected ConditionSession conditionSession;
 	/*------------------------------------------------*/
 
-
+//地図の表示のみを行う
 	@RequestMapping(path = "/menu/mapView" , method = RequestMethod.GET)
-	public ModelAndView map(Principal principal,
-			ModelAndView mav) {
+	public ModelAndView map(ModelAndView mav) {
 		mav.setViewName("map");
+		String condition = conditionSession.getCondition();
+		mav.addObject("data", condition);
+		return mav;
+	}
+	@RequestMapping(path="/sessionAdd/{placeId}/{placeName}/{lat}/{lng}" , method = RequestMethod.GET)
+	public String sessionadd(
+			@PathVariable String placeId,
+			@PathVariable String placeName,
+			@PathVariable String lat,
+			@PathVariable String lng ){
+		BigDecimal lat1 = new BigDecimal(lat);
+		BigDecimal lng1 = new BigDecimal(lng);
+		System.out.println(placeName);
+//		緯度経度、名前をセッションに保存
+		conditionSession.setLatitude(lat1);
+		conditionSession.setLongitude(lng1);
+		conditionSession.setPlacename(placeName);
+		return "redirect:/shopinfo/"+ placeId;
+	}
+//	検索条件の保存
+	@Component
+	@Scope(value= "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public class ConditionSession implements Serializable {
+	      private String condition;
+	      private String placename;
+	      private BigDecimal latitude;
+	      private BigDecimal longitude;
+	      public String getCondition() {
+	             return condition;
+	      }
+	      public void setCondition(String condition) {
+	             this.condition = condition;
+	      }
+	      public String getPlacename() {
+	             return placename;
+	      }
+	      public void setPlacename(String placename) {
+	             this.placename = placename;
+	      }
+	      public BigDecimal getLatitude() {
+	             return latitude;
+	      }
+	      public void setLatitude(BigDecimal latitude) {
+	             this.latitude = latitude;
+	      }
+	      public BigDecimal getLongitude() {
+	             return longitude;
+	      }
+	      public void setLongitude(BigDecimal longitude) {
+	             this.longitude = longitude;
+	      }
+	}
+//	検索時実行
+	@ResponseBody
+	@RequestMapping(path="/menu/search" , method = RequestMethod.POST, produces="application/json")
+	public Iterable<Shop> ajax(@RequestBody String formData,Principal principal,
+			Model mav) {
+//		検索条件保存
+		conditionSession.setCondition("Both");
+		if(formData.equals("\"locate=Exist\"")) {
+			conditionSession.setCondition("Exist");
+		}else if(formData.equals("\"locate=Exist&near=near\"")) {
+			conditionSession.setCondition("near");
+		}else if(formData.equals("\"locate=Exist&mylog=mylog\"")) {
+			conditionSession.setCondition("mylog");
+		}else if(formData.equals("\"locate=Exist&near=near&mylog=mylog\"")) {
+			conditionSession.setCondition("both condition");
+		}
+		Iterable<Shop> list = null;
+		return list;
+	}
+//	Map表示時及びMap操作時実行　画面内にあるか判定
+	@ResponseBody
+	@RequestMapping(path="/menu/border" , method = RequestMethod.POST, produces="application/json")
+	public Iterable<Shop> ajax2(@RequestBody List<String> test_arr,Principal principal,
+			Model mav) {
+		BigDecimal lat1 = new BigDecimal(test_arr.get(0));
+		BigDecimal lng1 = new BigDecimal(test_arr.get(1));
+		BigDecimal lat2 = new BigDecimal(test_arr.get(2));
+		BigDecimal lng2 = new BigDecimal(test_arr.get(3));
+		Iterable<Shop> list = null;
 //		ログイン情報の取得
 		Authentication auth = (Authentication) principal;
 		AccountDetails accountDetails = (AccountDetails) auth.getPrincipal();
@@ -70,245 +149,8 @@ public class MapsController {
 		Date date = calendar.getTime();
 //		セッション情報の取得
 		String condition = conditionSession.getCondition();
-		mav.addObject("data",condition);
-
-		System.out.println("session::" + condition);
-		System.out.println("session::" + conditionSession.getLatitude());
-		System.out.println("session::" + conditionSession.getLongitude());
-		System.out.println(conditionSession.getPlacename());
 //		検索条件の判定
-		if(condition == null || condition.equals("Both") || condition.equals("Exist")) {
-		Iterable<Shop> list = shoprepo.findAll();
-		mav.addObject("datalist", list);
-		}
-		else if(condition.equals("near")) {
-			List<String> time = footrepo.findByCreated_time((Date) date);
-			List<String> time2 = imagerepo.findByCreated_time((Date) date);
-			time2.addAll(time);
-			Set<String> set = new HashSet<>(time2);
-			System.out.println(set);
-			Iterable<Shop> neartime = shoprepo.findByid(set);
-			mav.addObject("datalist", neartime);
-		}else if(condition.equals("mylog")) {
-			List<String> logId =footrepo.findByLogin_id((String) Login_id);
-			List<String> logId2 =imagerepo.findByLogin_id((String) Login_id);
-			logId2.addAll(logId);
-			Set<String> set = new HashSet<>(logId2);
-			Iterable<Shop> mylog = shoprepo.findByid(set);
-			mav.addObject("datalist", mylog);
-		}else if(condition.equals("both condition")) {
-			List<String> con = footrepo.findByCreated_timeAndLogin_id((Date) date,(String)Login_id);
-			List<String> con2 = imagerepo.findByCreated_timeAndLogin_id((Date) date,(String)Login_id);
-			con2.addAll(con);
-			Set<String> set = new HashSet<>(con2);
-			Iterable<Shop> both = shoprepo.findByid(set);
-			mav.addObject("datalist",both );
-		}
-
-
-		return mav;
-	}
-
-	@RequestMapping(path="/menu/searchView" , method = RequestMethod.POST)
-	public String search(@RequestParam String locate, String near,String mylog) {
-//		検索条件をセッションの保存
-		conditionSession.setCondition(locate );
-		if(near != null && mylog == null) {
-			conditionSession.setCondition( near);
-		}else if(mylog != null && near == null) {
-			conditionSession.setCondition( mylog);
-		}else if(mylog != null && near !=null) {
-			conditionSession.setCondition("both condition");
-		}
-
-		return "redirect:mapView";
-	}
-
-	@RequestMapping(path="/sessionAdd/{placeId}/{placeName}/{lat}/{lng}" , method = RequestMethod.GET)
-	public String sessionadd(
-			@PathVariable String placeId,
-			@PathVariable String placeName,
-			@PathVariable String lat,
-			@PathVariable String lng ){
-
-		BigDecimal lat1 = new BigDecimal(lat);
-		BigDecimal lng1 = new BigDecimal(lng);
-//		double lat1 =Double.parseDouble(lat);
-//		double lng1 =Double.parseDouble(lng);
-		System.out.println(placeName);
-//		緯度経度、名前をセッションに保存
-		conditionSession.setLatitude(lat1);
-		conditionSession.setLongitude(lng1);
-		conditionSession.setPlacename(placeName);
-
-
-
-		return "redirect:/shopinfo/"+ placeId;
-	}
-
-
-	@Component
-	@Scope(value= "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
-	public class ConditionSession implements Serializable {
-
-	      private String condition;
-	      private String placename;
-	      private BigDecimal latitude;
-	      private BigDecimal longitude;
-
-
-
-	      public String getCondition() {
-	             return condition;
-	      }
-
-	      public void setCondition(String condition) {
-	             this.condition = condition;
-	      }
-	      public String getPlacename() {
-	             return placename;
-	      }
-
-	      public void setPlacename(String placename) {
-	             this.placename = placename;
-	      }
-	      public BigDecimal getLatitude() {
-	             return latitude;
-	      }
-
-	      public void setLatitude(BigDecimal latitude) {
-	             this.latitude = latitude;
-	      }
-	      public BigDecimal getLongitude() {
-	             return longitude;
-	      }
-
-	      public void setLongitude(BigDecimal longitude) {
-	             this.longitude = longitude;
-	      }
-
-	}
-//		ajax確認用
-	@ResponseBody
-	@RequestMapping(path="/menu/search" , method = RequestMethod.POST, produces="application/json")
-	public Iterable<Shop> ajax(@RequestBody String formData,Principal principal,
-			Model mav) {
-
-		conditionSession.setCondition("Both");
-		System.out.println("----------------");
-//		System.out.println("ajax=" +formData);
-		System.out.println("----------------");
-		if(formData.equals("\"locate=Exist\"")) {
-			System.out.println("ajax=Exist true" );
-			conditionSession.setCondition("Exist");
-			System.out.println("----------------");
-		}else if(formData.equals("\"locate=Exist&near=near\"")) {
-			System.out.println("ajax=locate=Exist&near=near" );
-			conditionSession.setCondition("near");
-			System.out.println("----------------");
-		}else if(formData.equals("\"locate=Exist&mylog=mylog\"")) {
-			System.out.println("ajax=Exist&mylog=mylog" );
-			conditionSession.setCondition("mylog");
-			System.out.println("----------------");
-		}else if(formData.equals("\"locate=Exist&near=near&mylog=mylog\"")) {
-			System.out.println("ajax=Exist&near=near&mylog=mylog" );
-			conditionSession.setCondition("both condition");
-			System.out.println("----------------");
-		}
-		Iterable<Shop> list = null;
-		Authentication auth = (Authentication) principal;
-		AccountDetails accountDetails = (AccountDetails) auth.getPrincipal();
-		String Login_id = accountDetails.getMember().getLogin_id();
-//		時間の取得
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DATE, -14);
-		Date date = calendar.getTime();
-//		セッション情報の取得
-		String condition = conditionSession.getCondition();
-//		mav.addObject("data",condition);
-
-		System.out.println("session::" + condition);
-		System.out.println("session::" + conditionSession.getLatitude());
-		System.out.println("session::" + conditionSession.getLongitude());
-		System.out.println(conditionSession.getPlacename());
-//		検索条件の判定
-		if(condition == null || condition.equals("Both") || condition.equals("Exist")) {
-		list = shoprepo.findAll();
-		System.out.println("-----------------------");
-		System.out.println(list);
-		System.out.println("-----------------------");
-//		mav.addAttribute("datalist", list);
-		}
-		else if(condition.equals("near")) {
-			List<String> time = footrepo.findByCreated_time((Date) date);
-			List<String> time2 = imagerepo.findByCreated_time((Date) date);
-			time2.addAll(time);
-			Set<String> set = new HashSet<>(time2);
-			System.out.println(set);
-			list = shoprepo.findByid(set);
-//			mav.addAttribute("datalist", neartime);
-		}else if(condition.equals("mylog")) {
-			List<String> logId =footrepo.findByLogin_id((String) Login_id);
-			List<String> logId2 =imagerepo.findByLogin_id((String) Login_id);
-			logId2.addAll(logId);
-			Set<String> set = new HashSet<>(logId2);
-			list = shoprepo.findByid(set);
-//			mav.addObject("datalist", mylog);
-		}else if(condition.equals("both condition")) {
-			List<String> con = footrepo.findByCreated_timeAndLogin_id((Date) date,(String)Login_id);
-			List<String> con2 = imagerepo.findByCreated_timeAndLogin_id((Date) date,(String)Login_id);
-			con2.addAll(con);
-			Set<String> set = new HashSet<>(con2);
-			list= shoprepo.findByid(set);
-//			mav.addObject("datalist",both );
-		}
-		System.out.println("-----------------------");
-		System.out.println(list.getClass());
-		System.out.println(list);
-		System.out.println("-----------------------");
-
-		return list;
-	}
-
-//	画面内か判定
-	@ResponseBody
-	@RequestMapping(path="/menu/border" , method = RequestMethod.POST, produces="application/json")
-	public Iterable<Shop> ajax2(@RequestBody List<String> test_arr,Principal principal,
-			Model mav) {
-		BigDecimal lat1 = new BigDecimal(test_arr.get(0));
-		BigDecimal lng1 = new BigDecimal(test_arr.get(1));
-		BigDecimal lat2 = new BigDecimal(test_arr.get(2));
-		BigDecimal lng2 = new BigDecimal(test_arr.get(3));
-		System.out.println(test_arr);
-		System.out.println(lat1);
-		System.out.println(lng1);
-		System.out.println(lat2);
-		System.out.println(lng2);
-		Iterable<Shop> list = null;
-
-		Authentication auth = (Authentication) principal;
-		AccountDetails accountDetails = (AccountDetails) auth.getPrincipal();
-		String Login_id = accountDetails.getMember().getLogin_id();
-//		時間の取得
-		Calendar calendar = Calendar.getInstance();
-		calendar.add(Calendar.DATE, -14);
-		Date date = calendar.getTime();
-//		セッション情報の取得
-		String condition = conditionSession.getCondition();
-//		mav.addObject("data",condition);
-
-		System.out.println("session::" + condition);
-		System.out.println("session::" + conditionSession.getLatitude());
-		System.out.println("session::" + conditionSession.getLongitude());
-		System.out.println(conditionSession.getPlacename());
-//		検索条件の判定
-		if(condition == null || condition.equals("Both") ) {
-		list = shoprepo.findAll();
-		System.out.println("-----------------------");
-		System.out.println(list);
-		System.out.println("-----------------------");
-		}
-		else if(condition.equals("Exist")) {
+		if(condition == null || condition.equals("Both")||condition.equals("Exist")) {
 			list = shoprepo.findBylatlng((BigDecimal) lat1,(BigDecimal) lat2,(BigDecimal) lng1,(BigDecimal) lng2);
 		}
 		else if(condition.equals("near")) {
@@ -316,7 +158,6 @@ public class MapsController {
 			List<String> time2 = imagerepo.findByCreated_time((Date) date);
 			time2.addAll(time);
 			Set<String> set = new HashSet<>(time2);
-			System.out.println(set);
 			list = shoprepo.findBylatlngAndId(set,(BigDecimal) lat1,(BigDecimal) lat2,(BigDecimal) lng1,(BigDecimal) lng2);
 		}else if(condition.equals("mylog")) {
 			List<String> logId =footrepo.findByLogin_id((String) Login_id);
@@ -331,13 +172,6 @@ public class MapsController {
 			Set<String> set = new HashSet<>(con2);
 			list = shoprepo.findBylatlngAndId(set,(BigDecimal) lat1,(BigDecimal) lat2,(BigDecimal) lng1,(BigDecimal) lng2);
 		}
-//		System.out.println("-----------------------");
-//		System.out.println(list.getClass());
-//		System.out.println(list);
-//		System.out.println("-----------------------");
-
-
-		return list;
+	return list;
 	}
-
 }
--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -6,24 +6,19 @@ function initMap(){
     	alert('Geolocation APIに対応していません');
     	return false;
     }
-
 	navigator.geolocation.getCurrentPosition(function(position){
 		var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);//現在地
 		var opts = {
 			zoom:19,
 			center:latlng
 		};
-
-		console.log(con);
 		map = new google.maps.Map(document.getElementById("map_sam"), opts);
-
 		//現在地にマーカーを立てる
 		var nowPlace = new google.maps.Marker({
 			position: latlng,
 			title: "現在地",
 			map: map
 		});
-
 		//開始&ドラッグ等操作完了時実行
 		google.maps.event.addListener(map, 'idle', function(){
 			dispLatLng();
@@ -31,12 +26,9 @@ function initMap(){
 	},function(){
 		alert('位置情報取得に失敗しました');
 	});
-
-
 }
 //ドラッグ操作の設定
 function dispLatLng(){
-
 	if(marker.length > 0){
 		//マーカー削除
 		for (var i = 0; i <  marker.length; i++) {
@@ -51,84 +43,29 @@ function dispLatLng(){
 	var center = map.getCenter();
 	var cenlat = center.lat();
 	var cenlng = center.lng();
-//	console.log(cenlat);
-//	console.log(cenlng);
-
 //	画面の境界を取得
 	var bounds = map.getBounds();
 //	地図の右上の座標
 	var map_ne = bounds.getNorthEast();
 	var nelat = map_ne.lat();
 	var nelng = map_ne.lng();
-
 //	地図の左下の座標
 	var map_sw = bounds.getSouthWest();
 	var swlat = map_sw.lat();
 	var swlng = map_sw.lng();
-
-/*ajaxで画面限界を送信、datalist更新----------------------*/
+/*ajaxで画面限界を送信、datalistを取得----------------------*/
 	border(nelat,nelng,swlat,swlng);
-//
-
-	//map起動時またはあり/なし両方が押されたとき
-	if(con == "Both" || con == null){
-		var request = {
-			location: center,
-			rankBy: google.maps.places.RankBy.DISTANCE,
-//				radius:500,
-			type:['restaurant']
-//				fields: ['name',  'place_id', 'geometry'] フィールド指定できない
-		};
-
-		console.log(request);
-		service = new google.maps.places.PlacesService(map);
-		service.nearbySearch(request, callback);
-
-
-	}else{
-		//ありが押されたとき
-		for(var i= 0;i<datalist.length;i++){
-//			console.log(datalist.length);
-			var latlng = new google.maps.LatLng(datalist[i].latitude,datalist[i].longitude ) ;
-			var res1 = distance(cenlat, cenlng, datalist[i].latitude, datalist[i].longitude);
-//			console.log("res1="+ res1);
-			var vis = true;
-			if(res1 > 500){
-				vis = false;
-			}
-//			console.log(datalist[i]);
-			marker.push ( new google.maps.Marker({
-				position:latlng,
-				title:datalist[i].place_name,
-				url:"/sessionAdd/" + datalist[i].place_id + "/" + datalist[i].place_name+"/"+datalist[i].latitude+"/"+datalist[i].longitude,//情報ページのurl指定
-				map: map,
-				visible:vis,
-				icon:'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
-
-			}));
-
-			google.maps.event.addListener(marker[i],'click',function(){
-				window.location.href = this.url;
-
-			})
-		}
-	}
 }
-
 //検索結果の処理
 function callback(results, status) {
 	if (status == google.maps.places.PlacesServiceStatus.OK) {
-		console.log(results);
 		for (var i = 0; i < results.length; i++) {
 			createMarker(results[i]);
 		}
 	}
 }
-
-
 //マーカーを表示
 function createMarker(place){
-//	console.log(place);
 	var center = map.getCenter();
 	var cenlat = center.lat();
 	var cenlng = center.lng();
@@ -136,27 +73,22 @@ function createMarker(place){
 		return;
 	}
 	//console.log(place.place_id);
-	var iconCo ='http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+	var iconCo ='https://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
 	var vis = true;
-
 	//place_idの判定
-	if(datalist != null){
-		for(var i = 0; i < datalist.length; i++){
-            if(place.place_id == datalist[i].place_id){
-            	iconCo = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
-				break;
-			}else{
-				iconCo ='https://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
-			}
+	for(var i = 0; i < datalist.length; i++){
+        if(place.place_id == datalist[i].place_id){
+        	iconCo = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+			break;
+		}else{
+			iconCo ='https://maps.google.com/mapfiles/ms/icons/yellow-dot.png';
 		}
 	}
-
 	//中心座標との距離
 	var lat = place.geometry.location.lat();
 	var lng = place.geometry.location.lng();
 	var res1 = distance(cenlat, cenlng, lat, lng)
 //	console.log("res1="+ res1);
-
 	if(res1 > 500){
 		vis = false;
 	}
@@ -173,14 +105,12 @@ function createMarker(place){
 		visible:vis,
 		icon:iconCo
 	}));
-
 	//クリック時urlに移動
 	for(var i=0; i < marker.length; i++){
 		google.maps.event.addListener(marker[i],'click',function(){
 			window.location.href = this.url;
 		});
 	}
-
 }
 //ページ遷移用メソッド
 function toView(to) {
@@ -196,7 +126,6 @@ function Modalclose(){
 	const modalArea = document.getElementById('modalArea');
 	modalArea.classList.remove('is-show')
 }
-
 //あり/なし両方を押したとき
 function flg0(ischecked){
 	const imp = document.getElementById("imp");
@@ -213,7 +142,6 @@ function flg0(ischecked){
 		imp2.disabled = false;
 	}
 }
-
 //ありを押したとき
 function flg1(ischecked){
 	const imp = document.getElementById("imp");
@@ -223,7 +151,7 @@ function flg1(ischecked){
 		imp.classList.remove('imp');
   		imp1.disabled = false;
   		imp2.disabled = false;
-	} else {
+	}else {
   		imp1.disabled = true;
   		imp2.disabled = true;
 	}
@@ -235,9 +163,8 @@ function distance(lat1, lng1, lat2, lng2) {
 	  lat2 *= Math.PI / 180;
 	  lng2 *= Math.PI / 180;
 	  return 6378137 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
-	}
-
-//ajax
+}
+//検索実行
 function search(){
 	/*ajaxでpostで送信するとき必要---------------------------------*/
     var token = $("meta[name='_csrf']").attr("content");
@@ -246,71 +173,29 @@ function search(){
       xhr.setRequestHeader(header, token);
     });
     /*------------------------------------------------------------*/
-
-
     var jsonString = $('#the-form').serialize();
 	var formData =  JSON.stringify(jsonString);
-
 	condition(formData);
-	console.log(con);
-//	 console.log(formData);
-	 $.ajax({
-       url: "/menu/search",  //POST送信を行うファイル名を指定
-       type: "POST",
-       data: formData,  //POST送信するデータを指定
-       datatype: 'json',
-       contentType: 'application/json',
-       scriptCharset: 'utf-8',
-       success: function(data) {
-    	   console.log("success");
-    	   console.log(data);
-    	   datalist = data;
-    	   initMap();
-    	   Modalclose();
-       },
-       error: function(data) {
-    	   alert('error');
-       }
-
+	$.ajax({
+	   url: "/menu/search",  //POST送信を行うファイル名を指定
+	   type: "POST",
+	   data: formData,  //POST送信するデータを指定
+	   datatype: 'json',
+	   contentType: 'application/json',
+	   scriptCharset: 'utf-8',
+	   success: function(data) {
+		   datalist = data;
+		   initMap();
+		   Modalclose();
+	   },
+	   error: function(data) {
+		   alert('error');
+	   }
    });
-
 }
-
+//境界をサーバーに送信
 function border(nelat,nelng,swlat,swlng){
-//	画面の境界を取得
-//	var bounds = map.getBounds();
-////	地図の右上の座標
-//	var map_ne = bounds.getNorthEast();
-//	var nelat = map_ne.lat();
-//	var nelng = map_ne.lng();
-//
-////	地図の左下の座標
-//	var map_sw = bounds.getSouthWest();
-//	var swlat = map_sw.lat();
-//	var swlng = map_sw.lng();
-	console.log(nelat);
-	console.log(nelng);
-	console.log(swlat);
-	console.log(swlng);
-//	var test_arr = new Array();
-//	test_arr["a"] = nelat;
-//	test_arr["b"] = nelng;
-//	test_arr["c"] = swlat;
-//	test_arr["d"] = swlng;
-
 	var test_arr =[nelat,nelng,swlat,swlng];
-//	test_arr.push = nelat;
-//	test_arr.push = nelng;
-//	test_arr.push = swlat;
-//	test_arr.push = swlng;
-
-console.log(test_arr);
-	//連想配列をobjectに変換
-	var test_obj = {};
-	for(key in test_arr){
-	  test_obj [key] = test_arr[key];
-	}
-
 	/*ajaxでpostで送信するとき必要---------------------------------*/
     var token = $("meta[name='_csrf']").attr("content");
     var header = $("meta[name='_csrf_header']").attr("content");
@@ -318,33 +203,57 @@ console.log(test_arr);
       xhr.setRequestHeader(header, token);
     });
     /*------------------------------------------------------------*/
-
+    var center = map.getCenter();
+	var cenlat = center.lat();
+	var cenlng = center.lng();
     $.ajax({
-        url: "/menu/border",  //POST送信を行うファイル名を指定
+    	url: "/menu/border",  //POST送信を行うファイル名を指定
         type: "POST",
         data: JSON.stringify(test_arr),  //POST送信するデータを指定
-        //data:{"test_obj" :test_arr }
         datatype: 'json',
         contentType: 'application/json',
         scriptCharset: 'utf-8',
         traditional: true,
         success: function(data) {
-     	   console.log("success");
-     	  console.log(datalist);
-     	   console.log(data);
      	   datalist = data;
-     	  console.log(datalist);
-//     	   initMap();
-//     	   Modalclose();
+     	   console.log(datalist);
+     		//map起動時またはあり/なし両方が押されたとき
+     		if(con == "Both" || con == null){
+     			var request = {
+     				location: center,
+     				rankBy: google.maps.places.RankBy.DISTANCE,
+     				type:['restaurant']
+     			};
+     			service = new google.maps.places.PlacesService(map);
+     			service.nearbySearch(request, callback);
+     		}else{
+     			//ありが押されたとき
+     			for(var i= 0;i<datalist.length;i++){
+     				var latlng = new google.maps.LatLng(datalist[i].latitude,datalist[i].longitude ) ;
+     				var res1 = distance(cenlat, cenlng, datalist[i].latitude, datalist[i].longitude);
+     				var vis = true;
+     				if(res1 > 500){
+     					vis = false;
+     				}
+     				marker.push ( new google.maps.Marker({
+     					position:latlng,
+     					title:datalist[i].place_name,
+     					url:"/sessionAdd/" + datalist[i].place_id + "/" + datalist[i].place_name+"/"+datalist[i].latitude+"/"+datalist[i].longitude,//情報ページのurl指定
+     					map: map,
+     					visible:vis,
+     					icon:'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+     				}));
+     				google.maps.event.addListener(marker[i],'click',function(){
+     					window.location.href = this.url;
+     				})
+     			}
+     		}
         },
         error: function(data) {
      	   alert('error' );
         }
-
     });
-
 }
-
 function condition(cond){
 	con = "Both";
 	if(cond == "\"locate=Exist\""){
@@ -356,5 +265,4 @@ function condition(cond){
 	}else if(cond == "\"locate=Exist&near=near&mylog=mylog\""){
 		con = "both condition";
 	}
-
 }
