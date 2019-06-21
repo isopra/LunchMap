4fce7a28f076f6f795999c7637aa20bc53b14a87
tomisaki.yui <tomisaki.yui@isopra.co.jp>
2019-06-21 16:14:55 +0900

    登録処理の実装

--- src/main/java/jp/co/isopra/lunchmap/controller/ImageController.java
+++ src/main/java/jp/co/isopra/lunchmap/controller/ImageController.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.security.Principal;
 import java.util.Date;
 import java.util.List;
 
@@ -17,9 +18,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import jp.co.isopra.lunchmap.controller.MapsController.ConditionSession;
 import jp.co.isopra.lunchmap.entity.AccountDetails;
 import jp.co.isopra.lunchmap.entity.Image;
 import jp.co.isopra.lunchmap.entity.Shop;
@@ -28,6 +31,7 @@ import jp.co.isopra.lunchmap.repositories.ShopRepository;
 
 
 @Controller
+@SessionAttributes("scopedTarget.ConditionSession")
 public class ImageController {
 
 	@Autowired
@@ -36,37 +40,42 @@ public class ImageController {
 	@Autowired
 	ShopRepository ShopRepository;
 
+	@Autowired
+	protected ConditionSession conditionSession;
+
 	// 画像登録画面
 	@RequestMapping(value= "create/image", method = RequestMethod.GET)
     ModelAndView uploadview(
     		ModelAndView mav,
     		@RequestParam String place_id,
-    		@RequestParam String place_name)
+    		Principal Principal,
+    		@AuthenticationPrincipal AccountDetails accountDetails)
 	{
 
 		mav.setViewName("image");
 	    mav.addObject("place_id", place_id);
-	    mav.addObject("place_name", place_name);
+	    mav.addObject("place_name", conditionSession.getPlacename());
 	    return mav;
 	}
 
-	// 登録処理 ToDo:DB保存繰り返し
+	// 登録処理
 	@RequestMapping(value="create/image/register", method = RequestMethod.POST)
 	String upload(
 			ModelAndView mav,
 			@RequestParam List<MultipartFile> files,
 			@RequestParam String place_id,
-			@RequestParam String place_name,
+			Principal Principal,
 			@AuthenticationPrincipal AccountDetails accountDetails
 			)
 	{
-		
+		String place_name = conditionSession.getPlacename();
+
 		for(MultipartFile file : files) {
-		
+
 			Image entity = new Image();
 			Date nowDate = new Date();
 			Shop entityShop;
-			
+
 			// ShopTableにinsert
 			if(this.ShopRepository.existsById(place_id)) {
 				entityShop = this.ShopRepository.findById(place_id).get();
@@ -74,33 +83,35 @@ public class ImageController {
 				entityShop = new Shop();
 				entityShop.setPlace_id(place_id);
 				entityShop.setPlace_name(place_name);
-				
+				entityShop.setLatitude(conditionSession.getLatitude());
+				entityShop.setLongitude(conditionSession.getLongitude());
+
 				ShopRepository.save(entityShop);
 			}
-			
+
 			// ImageTableにinsert
 			entity.setMember(accountDetails.getMember());
 			entity.setLogin_id(accountDetails.getMember().getLogin_id());
 			entity.setPlace_id(place_id);
 			entity.setCreated_time(nowDate);
 			entity.setShop(entityShop);
-			
+
 			imageRepository.save(entity);
-			
-			
+
+
 			// 画像をフォルダに保存
 			savefiles(files, place_id);
 		}
-		
-		return "image";
+
+		return "redirect:/shopinfo/" + place_id;
 	}
-	
+
 	/* 保存場所の指定 ToDo:ここでファイル名を変える*/
     private void savefile(MultipartFile file, String place_id) {
-    	  	
-    	String filename = file.getOriginalFilename(); 
-    	
-    	
+
+    	String filename = file.getOriginalFilename();
+
+
     	Path uploadfile = Paths.get("images/" + place_id + "/" + filename);
 
     	try (OutputStream os = Files.newOutputStream(uploadfile, StandardOpenOption.CREATE)) {
@@ -145,13 +156,14 @@ public class ImageController {
 		}
 
 	}
-	
-	// 戻るボタン
-	@RequestMapping(value = "menu/image/return", method = RequestMethod.POST)
+
+	// 戻るボタン TODO:前のページに戻るようにする
+	@RequestMapping(value = "/shopinfo", method = RequestMethod.POST)
 		public String previous(
-		@RequestParam String place_id,
-		@AuthenticationPrincipal AccountDetails accountDetails,
-		ModelAndView mav)
+				ModelAndView mav,
+				@RequestParam String place_id,
+				Principal Principal,
+				@AuthenticationPrincipal AccountDetails accountDetails)
 	{
 		return "redirect:/shopinfo/" + place_id;
 
